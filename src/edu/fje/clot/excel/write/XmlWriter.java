dd6f257608ef6303d2bd1dd61a35cae95a52a40c
MetalMor <srgark@gmail.com>
2017-05-01 18:50:44 +0200

    20170501: bug write

--- src/edu/fje/clot/excel/write/XmlWriter.java
+++ src/edu/fje/clot/excel/write/XmlWriter.java
@@ -3,10 +3,7 @@ package edu.fje.clot.excel.write;
 import edu.fje.clot.excel.Evaluation;
 import edu.fje.clot.excel.XmlFile;
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import jxl.Workbook;
 import jxl.write.Label;
 import jxl.write.WritableCellFormat;
@@ -51,6 +48,7 @@ public class XmlWriter implements XmlFile {
             }
         }
         try {
+            _wb.write();
             _wb.close();
         } catch (Exception ex) {
             System.err.println("Could not close XLS file.");
